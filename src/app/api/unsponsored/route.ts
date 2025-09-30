3bd8a2a3a52304b2bac070c648e26544baeef103
siMobin <shakibulislammobin@gmail.com>
2025-09-30 18:40:12 +0600

    try backend api

---
+++ src/app/api/unsponsored/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const response = await fetch(
+      "http://localhost:8000/api/students/unsponsored"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json(
+      { error: "An unknown error occurred" },
+      { status: 500 }
+    );
+  }
+}
